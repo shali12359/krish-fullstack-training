b205d6f20db697bc390366cbf5f58e42eabb93c8
IT17186766_Prabath <it17186766@my.sliit.lk>
2022-07-21 17:27:32 +0530

    spring cloud config server proj added

---
+++ demo-spring-cloud-config-store/demo-config-local/src/test/java/com/example/democonfiglocal/DemoConfigLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democonfiglocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
