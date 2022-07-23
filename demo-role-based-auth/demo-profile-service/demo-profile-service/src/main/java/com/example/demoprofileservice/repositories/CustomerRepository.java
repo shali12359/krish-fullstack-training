13c98f01825be7ed07adb8967f9b58321dd8013b
IT17186766_Prabath <it17186766@my.sliit.lk>
2022-07-24 00:44:00 +0530

    MS practile [11] new update

--- demo-role-based-auth/demo-profile-service/demo-profile-service/src/main/java/com/example/demoprofileservice/repositories/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demoprofileservice.repositories;
-
-import models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-}
